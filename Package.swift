d8a89b7028ffa93771964f2189e83a7feb0475cf
ParkingDrive <168358618+ParkingDrive@users.noreply.github.com>
2024-05-27 13:18:44 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "YandexMapsMobileWrapper",
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "YandexMapsMobileWrapper",
